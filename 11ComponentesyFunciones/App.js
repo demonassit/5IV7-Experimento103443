d33dcca2106e943dd216531a3e48a535d4fede30
demonassit <jminorg@ipn.mx>
2022-10-03 14:14:14 -0500

    componentes por funciones

--- 11ComponentesyFunciones/App.js
+++ 11ComponentesyFunciones/App.js
@@ -4,24 +4,22 @@
  * npx expo install react-native-web@~0.18.7 react-dom@18.0.0 @expo/webpack-config@^0.17.0
  */
 
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ import {ScrollView, StyleSheet} from 'react-native';
+ import SimpleState from "./components/functional/SimpleState";
+ 
+ export default function App() {
+     return (
+         <ScrollView>
+             <SimpleState/>
+         </ScrollView>
+     );
+ }
+ 
+ const styles = StyleSheet.create({
+     container: {
+         flex: 1,
+         backgroundColor: '#fff',
+         alignItems: 'center',
+         justifyContent: 'center',
+     },
+ });
